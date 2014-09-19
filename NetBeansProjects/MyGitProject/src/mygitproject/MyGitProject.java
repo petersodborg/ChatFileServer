8c8e3d4d21750ab489a0b1783c4489235937c145
petersodborg <sodborg@hotmail.com>
2014-09-19 13:31:11 +0200

    Hell yeah

--- NetBeansProjects/MyGitProject/src/mygitproject/MyGitProject.java
+++ NetBeansProjects/MyGitProject/src/mygitproject/MyGitProject.java
@@ -18,7 +18,7 @@ public class MyGitProject {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        
+        System.out.println("alloooha");
     }
     
 }
